b530c00fdb7159f018ef8476f276f4d3672e350a
Steve Blythe <steve.blythe@a8c.com>
2026-05-07 11:53:59 +0100

    Add date-added sort (Recently Added / Oldest Added)

--- update_all.py
+++ update_all.py
@@ -156,6 +156,7 @@ def page_to_album(page):
         "played": played, "last_played": get_date(props, "Last Played"),
         "direct_scrobble_url": get_url(props, "Direct Scrobble"),
         "rpm": get_select(props, "RPM") or "",
+        "date_added": page.get("created_time", ""),
     }
 
 
